afb9963741b5140de1a61b9963edc935c903c32d
Lennox  Galanje <takundagalanje@gmail.com>
2025-08-31 03:52:44 +0530

    stacked review cards

--- app.js
+++ app.js
@@ -41,3 +41,45 @@ faqItems.forEach((item) => {
     currentIcon.textContent = isOpen ? "+" : "-";
   });
 });
+
+// REVIEW CARDS
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".review__card");
+
+  function rotateCards() {
+    let angle = 0;
+
+   cards.forEach((card, index) => {
+          if (card.classList.contains("away")) {
+            card.style.transform = `translateY(-120vh) rotate(-48deg)`;
+          } else {
+            card.style.transform = ` rotate(${angle}deg)`;
+            angle = angle - 10;
+            card.style.zIndex = cards.length - index;
+          }
+        });
+      }
+
+      rotateCards();
+});
+
+ window.addEventListener("scroll", () => {
+   let distance = window.innerHeight * 0.5;
+
+   let topVal = stackArea.getBoundingClientRect().top;
+
+   let index = -1 * (topVal / distance + 1);
+
+   index = Math.floor(index);
+
+   for (i = 0; i < cards.length; i++) {
+     if (i <= index) {
+       cards[i].classList.add("away");
+     } else {
+       cards[i].classList.remove("away");
+     }
+   }
+   rotateCards();
+ });
+
+
