d1927d6f429115f18125a82b9a70901a24cbc9a7
Lennox  Galanje <takundagalanje@gmail.com>
2025-09-01 03:04:13 +0530

    centred stacked cards

--- app.js
+++ app.js
@@ -49,37 +49,20 @@ document.addEventListener("DOMContentLoaded", () => {
   function rotateCards() {
     let angle = 0;
 
-   cards.forEach((card, index) => {
-          if (card.classList.contains("away")) {
-            card.style.transform = `translateY(-120vh) rotate(-48deg)`;
-          } else {
-            card.style.transform = ` rotate(${angle}deg)`;
-            angle = angle - 10;
-            card.style.zIndex = cards.length - index;
-          }
-        });
+    cards.forEach((card, index) => {
+      if (card.classList.contains("away")) {
+        card.style.transform = `translate(-50%, -120vh) rotate(-48deg)`;
+      } else {
+        card.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+        angle -= 10;
+        card.style.zIndex = cards.length - index;
       }
+    });
+  }
 
-      rotateCards();
+  rotateCards();
 });
 
- window.addEventListener("scroll", () => {
-   let distance = window.innerHeight * 0.5;
 
-   let topVal = stackArea.getBoundingClientRect().top;
-
-   let index = -1 * (topVal / distance + 1);
-
-   index = Math.floor(index);
-
-   for (i = 0; i < cards.length; i++) {
-     if (i <= index) {
-       cards[i].classList.add("away");
-     } else {
-       cards[i].classList.remove("away");
-     }
-   }
-   rotateCards();
- });
 
 
